a127da8604ada6713a4adb13f36fc7c69ba73ee2
Saad Guessous <guessous_saad@hotmail.com>
2019-10-10 14:11:15 +0300

    added GET /users endpoint.

--- src/main/java/com/sguessou/app/ws/mobileappws/io/repositories/UserRepository.java
+++ src/main/java/com/sguessou/app/ws/mobileappws/io/repositories/UserRepository.java
@@ -6,5 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
     UserEntity findByEmail(String email);
+
+    UserEntity findByUserId(String userId);
 }
